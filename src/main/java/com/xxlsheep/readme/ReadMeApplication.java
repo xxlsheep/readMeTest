eef32516bd7445cefe4e62fe70088d7386039281
xxlsheep <eseuyun7@gmail.com>
2024-09-25 01:31:36 +0900

    first commit

---
+++ src/main/java/com/xxlsheep/readme/ReadMeApplication.java
@@ -0,0 +1,13 @@
+package com.xxlsheep.readme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxlsheep/readme/ReadMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxlsheep.readme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

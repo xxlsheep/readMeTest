3c017c9c60ab9f5426a7505f83b287216e2e6b1f
xxlsheep <eseuyun7@gmail.com>
2024-10-03 04:22:48 +0900

    test1

--- src/main/java/com/xxlsheep/readme/ReadMeApplication.java
+++ src/main/java/com/xxlsheep/readme/ReadMeApplication.java
@@ -8,6 +8,8 @@ public class ReadMeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadMeApplication.class, args);
+
+        System.out.println("test1");
     }
 
 }
